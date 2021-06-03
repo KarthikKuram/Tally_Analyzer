a50a88a0f601fb730a994a681d44fb7a153061eb
KarthikKuram <karthik.kurram@gmail.com>
2021-06-03 20:18:04 +0530

    Dashboard_First_cut_with_DataTable_and_Charts

--- js/app.js
+++ js/app.js
@@ -1,15 +1,65 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      title: "The Final Empire",
-      age: 45,
-    };
+// Bar chart
+new Chart(document.getElementById("line-chart"), {
+  type: "line",
+  data: {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        label: "Total Sales (Lakhs)",
+        backgroundColor: [
+          "#3e95cd",
+          "#8e5ea2",
+          "#3cba9f",
+          "#e8c3b9",
+          "#c45850",
+        ],
+        data: [2478, 5267, 734, 784, 433],
+        // fill=true,
+        borderColor: "#3e95cd",
+      },
+    ],
   },
-  methods: {
-    changeTitle() {
-      this.title = "Title Changed!";
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
+      text: "Predicted world population (millions) in 2050",
     },
   },
 });
 
-app.mount("#app");
+// Bar chart
+new Chart(document.getElementById("bar-chart"), {
+  type: "bar",
+  data: {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        label: "Total Expenses (Lakhs)",
+        backgroundColor: [
+          "#3e95cd",
+          "#8e5ea2",
+          "#3cba9f",
+          "#e8c3b9",
+          "#c45850",
+        ],
+        data: [1478, 2267, 734, 784, 433],
+      },
+    ],
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
+      text: "Predicted world population (millions) in 2050",
+    },
+  },
+});
+
+$(document).ready(function () {
+  $("#example").DataTable({
+    scrollY: "200px",
+    scrollCollapse: true,
+    paging: true,
+  });
+});
