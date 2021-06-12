019e1320f7588969d22b31bd81368448940b7b90
KarthikKuram <karthik.kurram@gmail.com>
2021-06-13 02:45:04 +0530

    DashBoard_Updated_More_Charts

--- js/app.js
+++ js/app.js
@@ -1,16 +1,59 @@
+// Random Number Generator
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+// Function for Colors
+function hexToRGBA(hex, opacity) {
+  return (
+    "rgba(" +
+    (hex = hex.replace("#", ""))
+      .match(new RegExp("(.{" + hex.length / 3 + "})", "g"))
+      .map(function (l) {
+        return parseInt(hex.length % 2 ? l + l : l, 16);
+      })
+      .concat(isFinite(opacity) ? opacity : 1)
+      .join(",") +
+    ")"
+  );
+}
+
+// Dashboard Theme colors
+// custom_colors = ["#0088c7", "#00a7d8", "#31c5e4", "#63e2ec", "#94fff4"];
+custom_colors = ["#0097dc", "#a18aeb", "#ff71c0", "#ff7670", "#ffa600"];
+
+// Dashboard Theme colors in RGBA
+var custom_colors_rgba = custom_colors.map(function (item) {
+  return hexToRGBA(item, 0.5);
+});
+
 // Line Chart for Sales Dash Card
 new Chart(document.getElementById("line-chart-dash"), {
   type: "line",
   data: {
-    labels: ["", "", "", "", "", "", "", "", "", "", "", ""],
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
     datasets: [
       {
-        data: [247, 526, 734, 584, 433, 478, 567, 734, 784, 433, 456, 297],
-        // data: [0, 0, 0, 0, 0],
-        borderColor: "grey",
+        data: [247, 526, 734, 584, 433, 478, 567, 734, 784, 733, 856, 997],
+        borderColor: custom_colors[0],
         fill: true,
-        backgroundColor: "#f5f3f4",
-        lineTension: 0.4,
+        backgroundColor: custom_colors_rgba[0],
+        lineTension: 0.5,
         pointRadius: 2,
         pointHoverRadius: 6,
       },
@@ -57,13 +100,7 @@ new Chart(document.getElementById("bar-chart-dash"), {
     ],
     datasets: [
       {
-        backgroundColor: [
-          "#3e95cd",
-          "#8e5ea2",
-          "#3cba9f",
-          "#e8c3b9",
-          "#c45850",
-        ],
+        backgroundColor: [...custom_colors],
         data: [1478, 2267, 734, 784, 433, 500, 340, 567, 789, 876, 777, 343],
       },
     ],
@@ -94,32 +131,13 @@ new Chart(document.getElementById("bar-chart-dash"), {
 new Chart(document.getElementById("donut-chart-dash"), {
   type: "doughnut",
   data: {
-    labels: ["Party-1", "Party-2", "Party-3", "Party-4", "Party-5"],
+    labels: ["Receivable", "Payable"],
     datasets: [
       {
-        data: [47, 19, 71, 51, 22],
-        backgroundColor: [
-          "rgba(216, 17, 89, 0.5)",
-          "rgba(143, 45, 86, 0.5)",
-          "rgba(33, 131, 128, 0.5)",
-          "rgba(251, 177, 60, 0.5)",
-          "rgba(115, 210, 222, 0.5)",
-        ],
-        borderWidth: 2,
+        data: [47, 19],
+        backgroundColor: [...custom_colors],
+        borderWidth: 3,
         radius: 50,
-      },
-      {
-        data: [25, 25, 25, 20, 22],
-        backgroundColor: [
-          "rgba(255, 166, 158, 1)",
-          "rgba(94, 100, 114, 1)",
-          "rgba(250, 243, 221, 1)",
-          "rgba(184, 242, 230, 1)",
-          "rgba(174, 217, 224, 1)",
-        ],
-        borderColor: "rgba(37, 9, 2, 0.2)",
-        borderWidth: 1,
-        radius: 45,
       },
     ],
   },
@@ -132,8 +150,99 @@ new Chart(document.getElementById("donut-chart-dash"), {
       },
     },
     borderAlign: "inner",
-    cutout: 20,
-    // radius: 50,
+    cutout: 40,
+  },
+});
+
+// Pie Chart for Cash and Bank Dash Card
+new Chart(document.getElementById("pie-chart-dash"), {
+  type: "pie",
+  data: {
+    labels: ["Bank-In", "Bank-Out", "Cash-In", "Cash-Out"],
+    datasets: [
+      {
+        data: [300, 150, 120, 150],
+        backgroundColor: [
+          custom_colors[0],
+          custom_colors[1],
+          custom_colors[0],
+          custom_colors[1],
+        ],
+        borderAlign: "inner",
+        borderColor: [
+          custom_colors[0],
+          custom_colors[0],
+          custom_colors[4],
+          custom_colors[4],
+        ],
+        radius: 50,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+      title: {
+        display: false,
+      },
+    },
+  },
+});
+
+// WaterFall Chart for P&L Movement
+
+new Chart(document.getElementById("waterfall"), {
+  type: "bar",
+  data: {
+    labels: [
+      "Revenue",
+      "Direct Expenses",
+      "Gross Profit",
+      "Indirect Expenses",
+      "Net Profit",
+    ],
+    datasets: [
+      {
+        data: [
+          [0, 5400000],
+          [3000000, 5400000],
+          [0, 3000000],
+          [2000000, 3000000],
+          [0, 2000000],
+        ],
+        backgroundColor: [...custom_colors],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        grid: { display: false, drawBorder: true },
+        ticks: {
+          display: true,
+          autoSkip: false,
+          padding: 3,
+          maxRotation: 0,
+          font: { size: 10 },
+        },
+      },
+      y: {
+        grid: { display: false, drawBorder: false },
+        ticks: {
+          display: false,
+        },
+      },
+    },
   },
 });
 
@@ -152,14 +261,8 @@ new Chart(document.getElementById("donut-top-5-Customers"), {
     datasets: [
       {
         data: [47, 19, 71, 51, 22],
-        backgroundColor: [
-          "rgba(248, 198, 149, 1)",
-          "rgba(240, 171, 172, 1)",
-          "rgba(186, 219, 216, 1)",
-          "rgba(166, 188, 211, 1)",
-          "rgba(115, 210, 222, 1)",
-        ],
-        borderWidth: 3,
+        backgroundColor: [...custom_colors],
+        borderWidth: 4,
       },
     ],
   },
@@ -176,59 +279,359 @@ new Chart(document.getElementById("donut-top-5-Customers"), {
   },
 });
 
-// line chart
-new Chart(document.getElementById("line-charts"), {
-  type: "line",
+// Top 5 Vendors
+new Chart(document.getElementById("h-bar-top-5-expenses"), {
+  type: "bar",
   data: {
-    labels: ["January", "February", "March", "April", "May"],
+    labels: ["Party-1", "Party-2", "Party-3", "Party-4", "Party-5"],
     datasets: [
       {
-        label: "Total Sales (Lakhs)",
-        backgroundColor: [
-          "#3e95cd",
-          "#8e5ea2",
-          "#3cba9f",
-          "#e8c3b9",
-          "#c45850",
-        ],
-        data: [2478, 5267, 734, 784, 433],
-        // fill=true,
-        borderColor: "#3e95cd",
+        backgroundColor: [...custom_colors],
+
+        data: [8000, 5000, 3000, 2000, 1000],
+        borderColor: [...custom_colors],
       },
     ],
   },
   options: {
-    legend: { display: false },
-    title: {
-      display: true,
+    indexAxis: "y",
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    plugins: {
+      legend: { display: false },
+    },
+    scales: {
+      x: {
+        grid: { display: false, drawBorder: false },
+        ticks: { display: false },
+      },
+      y: {
+        grid: { display: false, drawBorder: false },
+        ticks: { display: false },
+      },
     },
   },
 });
 
-// Bar chart
-new Chart(document.getElementById("bar-chart"), {
-  type: "bar",
+// Top 5 Sales Accounts
+new Chart(document.getElementById("pie-top-5-incomes"), {
+  type: "pie",
   data: {
-    labels: ["January", "February", "March", "April", "May"],
+    labels: ["Income-1", "Income-2", "Income-3", "Income-4", "Income-5"],
     datasets: [
       {
-        label: "Total Expenses (Lakhs)",
-        backgroundColor: [
-          "#3e95cd",
-          "#8e5ea2",
-          "#3cba9f",
-          "#e8c3b9",
-          "#c45850",
-        ],
-        data: [1478, 2267, 734, 784, 433],
+        backgroundColor: [...custom_colors],
+        data: [12, 19, 10, 17, 28],
+        borderWidth: 2,
       },
     ],
   },
   options: {
-    legend: { display: false },
+    repsonsive: true,
     title: {
-      display: true,
-      text: "Predicted world population (millions) in 2050",
+      display: false,
+    },
+    plugins: {
+      legend: false,
+    },
+  },
+});
+
+// Top 5 Expense Accounts
+new Chart(document.getElementById("polar-top-5-expenses"), {
+  type: "polarArea",
+  data: {
+    labels: ["Expense-1", "Expense-2", "Expense-3", "Expense-4", "Expense-5"],
+    datasets: [
+      {
+        data: [50, 40, 30, 20, 10],
+        backgroundColor: [...custom_colors],
+        borderWidth: 5,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+    scales: {
+      r: {
+        angleLines: { display: false },
+        ticks: { display: false },
+        grid: { display: true },
+      },
+      x: {
+        grid: { display: false, drawBorder: false },
+        ticks: {
+          display: false,
+          autoSkip: false,
+          padding: 0,
+          maxRotation: 0,
+          font: { size: 10 },
+        },
+      },
+      y: {
+        grid: { display: false, drawBorder: false },
+        ticks: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+// Receivable vs Payable Ageing
+new Chart(document.getElementById("bar-ageing"), {
+  type: "bar",
+  data: {
+    labels: ["0-30", "31-60", "61-90", "90-120", "> 120"],
+    datasets: [
+      {
+        label: "Receivable",
+        data: [50, 90, 30, 70, 10],
+        backgroundColor: custom_colors[0],
+        borderRadius: 5,
+      },
+      {
+        label: "Payable",
+        data: [120, 50, 25, 90, 35],
+        backgroundColor: custom_colors[4],
+        borderRadius: 5,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        grid: { display: true, drawBorder: true },
+        ticks: {
+          display: true,
+          autoSkip: false,
+          padding: 0,
+          maxRotation: 0,
+          font: { size: 10 },
+        },
+      },
+      y: {
+        grid: { display: false, drawBorder: false },
+        ticks: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+// Area chart for Sales v/s Purchase
+new Chart(document.getElementById("area-chart-accounts"), {
+  type: "line",
+  data: {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+    datasets: [
+      {
+        label: "Sales",
+        data: [247, 526, 734, 584, 433, 478, 567, 734, 784, 733, 856, 997],
+        borderColor: custom_colors_rgba[0],
+        fill: true,
+        backgroundColor: custom_colors_rgba[0],
+        lineTension: 0.3,
+        pointRadius: 2,
+        pointHoverRadius: 6,
+      },
+      {
+        label: "Purchases",
+        data: [200, 100, 800, 300, 200, 150, 600, 300, 350, 600, 220, 440],
+        borderColor: custom_colors_rgba[4],
+        fill: true,
+        backgroundColor: custom_colors_rgba[4],
+        lineTension: 0.3,
+        pointRadius: 2,
+        pointHoverRadius: 6,
+      },
+    ],
+  },
+  options: {
+    repsonsive: true,
+    title: {
+      display: false,
+    },
+    plugins: {
+      legend: false,
+    },
+    scales: {
+      x: {
+        grid: { display: false, drawBorder: false },
+        ticks: { display: false },
+      },
+      y: {
+        grid: { display: false, drawBorder: false },
+        ticks: { display: false },
+      },
+    },
+  },
+});
+
+// Bar Chart for Receivable Ageing
+new Chart(document.getElementById("bar-trend-accounts"), {
+  type: "bar",
+  data: {
+    labels: ["April", "May", "June", "July", "August", "September"],
+    datasets: [
+      {
+        label: "Sales",
+        data: [
+          getRandomIntInclusive(20000, 500000),
+          getRandomIntInclusive(20000, 500000),
+          getRandomIntInclusive(20000, 500000),
+          getRandomIntInclusive(20000, 500000),
+          getRandomIntInclusive(20000, 500000),
+          getRandomIntInclusive(20000, 500000),
+        ],
+        backgroundColor: custom_colors[0],
+      },
+      {
+        label: "Purchases",
+        data: [
+          getRandomIntInclusive(10000, 400000),
+          getRandomIntInclusive(10000, 400000),
+          getRandomIntInclusive(10000, 400000),
+          getRandomIntInclusive(10000, 400000),
+          getRandomIntInclusive(10000, 400000),
+          getRandomIntInclusive(10000, 400000),
+        ],
+        backgroundColor: custom_colors[1],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        grid: { display: true, drawBorder: true },
+        ticks: {
+          display: true,
+          autoSkip: false,
+          padding: 3,
+          maxRotation: 0,
+          font: { size: 10 },
+        },
+      },
+      y: {
+        grid: { display: false, drawBorder: false },
+        ticks: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+// Radar Chart for Top 3 Sales Account
+new Chart(document.getElementById("radar-top-5-sales"), {
+  type: "radar",
+  data: {
+    labels: ["April", "May", "June", "July", "August", "September"],
+    datasets: [
+      {
+        label: "Sales_Ac-1",
+        backgroundColor: custom_colors_rgba[0],
+        data: [
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+        ],
+      },
+      {
+        label: "Sales_Ac-2",
+        backgroundColor: custom_colors_rgba[1],
+        data: [
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+        ],
+      },
+      {
+        label: "Sales_Ac-3",
+        backgroundColor: custom_colors_rgba[2],
+        data: [
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(50000, 450000),
+          getRandomIntInclusive(10000, 700000),
+          getRandomIntInclusive(10000, 700000),
+        ],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+    scales: {
+      r: {
+        angleLines: { display: true },
+        ticks: { display: false },
+      },
+      x: {
+        grid: { display: false, drawBorder: false },
+        ticks: { display: false },
+      },
+      y: {
+        grid: { display: false, drawBorder: false },
+        ticks: { display: false },
+      },
     },
   },
 });
@@ -240,4 +643,14 @@ $(document).ready(function () {
     scrollCollapse: true,
     paging: true,
   });
+});
+
+// Change color of spans based on theme chosen:
+
+$(document).ready(function () {
+  $(".first_value").css("background", custom_colors[0]);
+  $(".second_value").css("background", custom_colors[1]);
+  $(".third_value").css("background", custom_colors[2]);
+  $(".fourth_value").css("background", custom_colors[3]);
+  $(".fifth_value").css("background", custom_colors[4]);
 });
